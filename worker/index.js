8440bd64ab38d5aaf751b780196297124b05f45d
Nathan Tulloch <nate@nathans-imac.home>
2020-05-05 15:46:39 -0400

    commit client

--- worker/index.js
+++ worker/index.js
@@ -11,12 +11,12 @@ const sub = redisClient.duplicate();
 
 function fib (index){
     if (index <2) return 1;
-    return fib (index - 1) + fib(index - 2)
+    return fib(index - 1) + fib(index - 2)
 }
 //subscribe and watch when a new value entered. this is a call back function
 sub.on('message',(channel,message)=>{
     //after calculating the value store it in a hash of values
     //message is the index value
-    redisClient.hset('values',message,fib(parseInt(message)))
+    redisClient.hset('values',message,fib(parseInt(message)));
 });
 sub.subscribe('insert');
