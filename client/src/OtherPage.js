886440f1053ce51234a89d0c023147bde3467f23
Nathan Tulloch <nate@nathans-imac.home>
2020-05-05 16:03:44 -0400

    client info

---
+++ client/src/OtherPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+export default() => {
+    return(
+        <div>
+            Nate Tulloch second page!
+            <Link to="/">GO back to home</Link>
+        </div>
+    );
+};
+
+
