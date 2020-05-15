d9657a0e28ac728be66ca075c7294b06d650040d
nmtulloch27 <nate@Nathans-iMac.local>
2020-05-15 16:34:24 -0400

    done

--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom';
 export default() => {
     return(
         <div>
-            Nate M Tulloch Second page!
+            Nate M Tulloch Second page Done!
             <Link to="/">GO back to home</Link>
         </div>
     );
